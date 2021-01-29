6ed086f3723981d12dd15f9308c63f2991c9a661
“Karla <karlamariztolentino@gmail.com>
2021-01-29 17:10:51 +1100

    added TTY-prompt gem, created menu options in index.rb

--- src/index.rb
+++ src/index.rb
@@ -2,3 +2,33 @@
 # DATA: history of haircuts, date, style
 # OUTPUT: suggested haircut style, recommended next haircut appointment
 
+require "tty-prompt"
+
+prompt = TTY::Prompt.new
+
+while true
+   choice = prompt.select(
+       "MENU", 
+       [
+           "✨ Give me a new hairstyle!",
+           "📝 Record last haircut",
+           "Generate random hair advice",
+           "Exit"
+       ])
+
+   if choice == "✨ Give me a new hairstyle!"
+       new_style = prompt.select(
+           "How short is your current hair?",
+           [
+               
+           ])
+
+       puts allergy_test(allergy_choice, input)
+   elsif choice == "Full allergy List"
+       puts "You are allergic to: #{get_allergies(input)}."
+   elsif choice == "Exit"
+       exit
+   else
+       p "Invalid choice"
+   end
+end
