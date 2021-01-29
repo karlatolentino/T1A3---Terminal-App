33265863b2ff16cdf2a2b6c55d22cd4be5fb9aa8
“Karla <karlamariztolentino@gmail.com>
2021-01-29 16:52:54 +1100

    updated flowchart diagram

--- src/index.rb
+++ src/index.rb
@@ -1,3 +1,4 @@
-# INPUT: Menu choice, hair questionnaire 
+# INPUT: Menu choice, hair questionnaire, record hair history, loop to go back to menu, prompt to quit
 # DATA: history of haircuts, date, style
 # OUTPUT: suggested haircut style, recommended next haircut appointment
+
